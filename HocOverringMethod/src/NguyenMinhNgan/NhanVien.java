048a741bb0fba6412808e82a2e16e78c1a1c7e25
NGAN <NGAN@ngmhngan>
2020-03-05 13:45:02 +0700

    d

---
+++ HocOverringMethod/src/NguyenMinhNgan/NhanVien.java
@@ -0,0 +1,11 @@
+package NguyenMinhNgan;
+
+public class NhanVien {
+	protected int ma;
+	protected String ten;
+	public abstract double tinhLuong();
+	public void funX()
+	{
+		
+	}
+}
---
+++ HocOverringMethod/src/module-info.java
@@ -0,0 +1,2 @@
+module HocOverringMethod {
+}
